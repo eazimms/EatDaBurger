5556bc2126cef0bae071a7c0ff5c3072e8cfca5f
eazimms <eric.allen.zimmerman@gmail.com>
2019-12-03 13:24:39 -0800

    finished

--- EatDaBurger/controllers/burgerController.js
+++ EatDaBurger/controllers/burgerController.js
@@ -15,25 +15,40 @@ router.get("/", function(req, res){
 }); 
 
 router.post("/api/burgers", function (req, res){
-  burger.insertOne(["name", "devoured"], [req.body.name, req.body.devoured], function(result){
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result){
     res.json({id: result.insertId}); 
   });
 }); 
 
-router.put("/api/burgers/:id", function (req, res) {
-  var condition = "id =" + req.params.id; 
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id; 
+
+  burger.deleteOne(condition, function(result) {
+    if(result.changedRows === 0) {
+      return res.status(404).end(); 
+    }
+
+    else {
+      res.status(200).end(); 
+    }
+  }); 
+}); 
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
 
   burger.updateOne({
     devoured: req.body.devoured
-  },
-  condition, 
-  function(result){
-    if(result.changedRows === 0){
-      return res.status(404).end(); 
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
-    res.status(200).end(); 
-  }
-  ); 
-}); 
+  });
+});
 
 module.exports = router; 
